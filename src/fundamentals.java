5685636b22d648466bd32830c973a2eca953e977
Alex <aleksf16#mail.ru>
2020-11-11 01:39:47 +0300

    1day3

--- src/fundamentals.java
+++ src/fundamentals.java
@@ -4,7 +4,18 @@ public class fundamentals {
     public static void main(String[] args) {
         Scanner in  = new Scanner(System.in);
         String name;
+        int arr[] = new int[4];
+
+
+        for(int i =0 ; i< arr.length; i++)
+        {
+            arr[i] = in.nextINT();
+        }
         name = in.nextLine();
         System.out.println("Your name18`72634781`26 " + name);
+        for (int i = 0 ; i < arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
     }
 }
